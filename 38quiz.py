64a5c187f41634e05472b901796a6a5a47881be2
marlel <mlamountry@ucdavis.edu>
2024-02-06 09:26:51 -0800

    up

--- 38quiz.py
+++ 38quiz.py
@@ -1,2 +1,16 @@
 # 38quiz.py by Marle Lamountry
 #Co-author: Addie Ferrer
+
+#Task 1
+def pi_estimate(n):
+	x = 1
+	sign = -1
+	for i in range(3, n, 2):
+		equation = sign * (1/n)
+		sign = sign * -1
+		x = x + equation
+		pi = 4 * (x)
+	return pi
+	
+print(pi_estimate(4))
+print(pi_estimate(12))
