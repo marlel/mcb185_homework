a8422a9503483c31e3be3ac5a66fd76e9e9a1c4e
marlel <mlamountry@ucdavis.edu>
2024-02-06 09:30:26 -0800

    up

--- 38quiz.py
+++ 38quiz.py
@@ -14,3 +14,17 @@ def pi_estimate(n):
 	
 print(pi_estimate(4))
 print(pi_estimate(12))
+
+#Task 2
+def nilakantha(limit):
+	sign_flip = 1
+	estimation = 3
+	for i in range(1, limit):
+		denom = 2 * i * (2 * i + 1) * (2 * i + 2)
+		equation = 4 / denom
+		estimation = estimation + (sign_flip * equation)
+		sign_flip = sign_flip * -1
+	return estimation
+
+print(pi_estimate(100), nilakantha(100))
+	
